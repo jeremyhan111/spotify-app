7dd0f6da3d6be088527ccfadb8e114b59e5c69f7
Jeremy Han <jeremyhan111@gmail.com>
2018-02-22 23:16:05 +0800

    set up mlab db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
 const local = "mongodb://localhost:27017/SpotifyApp";
-mongoose.connect(local);
+const online = `mongodb://${keys.dbuser}:${keys.dbpw}@ds143738.mlab.com:43738/spotify-app`
+mongoose.connect(online);
 
 module.exports = { mongoose };
