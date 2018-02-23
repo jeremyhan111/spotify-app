77a64d704f91dd77f6fac62614423372448dc2d1
Jeremy Han <jeremyhan111@gmail.com>
2018-02-23 10:10:27 +0800

    forgot to commit one

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,4 @@
 module.exports = {
-	spotifyClientID: 'f7d2545a5c5e4831a37fa2ac24a9ced6',
-	spotifyClientSecret: 'bcd1eac8efce448189e05c148774cb3b',
-	cookieKey: 'lklksjiuiutgllwkejrlkmncvballjkhlksjk4jr4uoii',
-	dbuser: 'spotifyappprod',
-	dbpw: 'spotifyappprod'
-};
-
-module.exports = {
 	spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
 	spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 	cookieKey: process.env.COOKIE_KEY,
