7a8451e43e75ddbce12fa70250e018d8000d3f15
Jeremy Han <jeremyhan111@gmail.com>
2018-02-23 11:53:39 +0800

    db updates

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
-const local = "mongodb://localhost:27017/SpotifyApp";
-const online = `mongodb://${keys.dbuser}:${keys.dbpw}@ds163745.mlab.com:63745/spotify-app-prod`
 
-mongoose.connect(online);
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+	db = `mongodb://${keys.dbuser}:${keys.dbpw}@ds163745.mlab.com:63745/spotify-app-prod`
+} else {
+	db = "mongodb://localhost:27017/SpotifyApp";	
+}
+
+mongoose.connect(db);
 
 module.exports = { mongoose };
